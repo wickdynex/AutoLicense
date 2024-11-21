6ba5cac73af6bda796147e441380aada647034a2
YiXuan Ding <1328032567@qq.com>
2024-11-21 18:46:42 +0800

    <add>: add `auto_license.py`.

--- src/auto_license.py
+++ src/auto_license.py
@@ -1,23 +1,28 @@
 import os
 import argparse
+from src.license_arg_config import LicenseArgConfig
+from src.license_generator import LicenseGenerator
+from src.license_manager import LicenseManager
 
-def add_license_header(file_path, header_text):
-    with open(file_path, 'r+') as f:
-        content = f.read()
-        if header_text not in content:
-            f.seek(0, 0)
-            f.write(header_text + '\n' + content)
+def auto_license():
+    config = LicenseArgConfig()
+    config.parse()
+    config.display_info()
 
-def process_directory(directory, header_text):
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith('.js') or file.endswith('.py'):  # 可以根据需求扩展文件类型
-                add_license_header(os.path.join(root, file), header_text)
+    generator = LicenseGenerator(config.license_file, 
+                                 config.license_type,
+                                 config.start_year, 
+                                 config.end_year, 
+                                 config.author)
+    license_text = generator.generate_license()
+
+    license_manager = LicenseManager(license_text, config.detail)
+
+    for root, dirs, files in os.walk(config.target_folder):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            license_manager.check_and_add_license(file_path)
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Add license header to files.')
-    parser.add_argument('directory', type=str, help='The directory to process.')
-    parser.add_argument('header', type=str, help='The license header to add.')
-    args = parser.parse_args()
-
-    process_directory(args.directory, args.header)
+    auto_license()
