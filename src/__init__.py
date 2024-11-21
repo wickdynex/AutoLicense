f1c5dd038c29aabf60e1490a9c8b696a2230f534
YiXuan Ding <1328032567@qq.com>
2024-11-21 19:20:44 +0800

    <add>: add `license` to head comment.

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,16 @@
+# MIT License
+# 
+# Copyright (c) 2024 - 2024 Wick Dynex
+# 
+# Permission is hereby granted, free of charge,
+# to any person obtaining a copy of this software and associated documentation files (the 'Software'),
+# to deal in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software
+# and to permit persons to whom the Software is furnished to do so
+# 
+# The above copyright notice
+# and this permission notice
+# shall be included in all copies or substantial portions of the Software.
 from .license_arg_config import LicenseArgConfig
 from .license_generator import LicenseGenerator
 from .license_manager import LicenseManager
