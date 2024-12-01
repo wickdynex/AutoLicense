04ade39caf35ddb5ed442188bd92503704d21326
YiXuan Ding <1328032567@qq.com>
2024-12-02 00:12:03 +0800

    <refactor>: remove unused variable, add docstring.

--- src/auto_license.py
+++ src/auto_license.py
@@ -3,7 +3,8 @@
 # Copyright (c) 2024 - 2024 Wick Dynex
 #
 # Permission is hereby granted, free of charge,
-# to any person obtaining a copy of this software and associated documentation files (the 'Software'),
+# to any person obtaining a copy of this software and associated documentation files
+# (the 'Software'),
 # to deal in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software
 # and to permit persons to whom the Software is furnished to do so
@@ -11,7 +12,21 @@
 # The above copyright notice
 # and this permission notice
 # shall be included in all copies or substantial portions of the Software.
-import argparse
+"""
+Automatically adds license headers to project files.
+
+This function:
+1. Parses and validates license-related arguments.
+2. Generates the license text based on the provided configuration.
+3. Applies the generated license to each file in the target folder.
+
+It iterates through all files in the specified folder and checks if the license header is 
+already present. If not, it adds the appropriate license header.
+
+Usage:
+    Call this function to automate license header management for project files.
+"""
+
 import os
 
 from src.license_arg_config import LicenseArgConfig
@@ -35,7 +50,7 @@ def auto_license():
 
     license_manager = LicenseManager(license_text, config.detail)
 
-    for root, dirs, files in os.walk(config.target_folder):
+    for root, files in os.walk(config.target_folder):
         for file_name in files:
             file_path = os.path.join(root, file_name)
             license_manager.check_and_add_license(file_path)
